1a7ba6056653200ef0793d520ce876ed39c04299
lxlWORK <460668853@qq.com>
2017-08-03 09:09:51 +0800

    aop

--- SpringAOP/src/main/java/com/lxl/SpringAOP/App.java
+++ SpringAOP/src/main/java/com/lxl/SpringAOP/App.java
@@ -1,13 +1,20 @@
 package com.lxl.SpringAOP;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+   
+    public static void main( String[] args )  
+    {  
+        System.out.println( "Hello Spring AOP!" );  
+        BeanFactory factory=new ClassPathXmlApplicationContext("applicationContext.xml");  
+        CustomerManager customerManager=(CustomerManager) factory.getBean("customerManager");  
+        customerManager.getCustomerById(2015);  
     }
 }
---
+++ SpringAOP/src/main/java/com/lxl/SpringAOP/CustomerManager.java
@@ -0,0 +1,8 @@
+package com.lxl.SpringAOP;
+
+public interface CustomerManager {
+    public void addCustomer(String name,String password);  
+    public void deleteCustomer(String name);  
+    public String getCustomerById(int id);  
+    public void updateCustomer(int id,String name,String password);  
+}
